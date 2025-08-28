4be9cf47931ddf3471370f52b0acd76cde202401
SophoGurabanidze <sogurabanidze@gmail.com>
2025-08-28 16:38:05 +0400

    Initial commit: set up Next.js app with Clerk and Prisma Neon DB

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Guest from '@/components/Guest'
+import { currentUser } from '@clerk/nextjs/server';
+
+export default async function HomePage(){
+const user= await currentUser();
+
+if(!user){
+  return <Guest/>;
+}
+  return (
+    <div>Homepage
+
+
+    </div>
+  )
+}
+
